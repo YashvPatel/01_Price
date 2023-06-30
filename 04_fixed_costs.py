3f0d6345dadaabe7762eda2b1ea8ce7608b4659c
YashvPatel <89170208+YashvPatel@users.noreply.github.com>
2023-07-01 10:58:14 +1200

    Add files via upload

---
+++ 04_fixed_costs.py
@@ -0,0 +1,81 @@
+# yes / no checker, (simple)
+def yes_no(question):
+    while True:
+        response = input(question)
+
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("Please enter either yes or no...\n")
+
+
+# Ask user for profit goal
+def profit_goal(total_costs):
+    # Initialise variables and error message
+    error = "Please enter a valid weight\n"
+
+    while True:
+
+        # ask for profit goal...
+        response = input("what is valid weight (eg 5000g or 5kgs) ")
+
+        # check if first character is $...
+        if response[0] == "$":
+            profit_type = "$"
+            # Get amount (everything after the $)
+            amount = response[1:]
+
+        # check if last character is %
+        elif response[-1] == "%":
+            profit_type = "%"
+            # Get amount (everything before the %)
+            amount = response[:-1]
+
+        else:
+            # set response to amount for now
+            profit_type = "unknown"
+            amount = response
+
+        try:
+            # Check amount is a number more than zero...
+            amount = float(amount)
+            if amount <= 0:
+                print(error)
+                continue
+
+        except ValueError:
+            print(error)
+            continue
+
+        if profit_type == "unknown" and amount >= 100:
+            dollar_type = yes_no("Do you mean {:.2f}g. "
+                                 "ie {:.2f} Grams?, "
+                                 "y / n ".format(amount, amount))
+
+            # Set profit type based on user answer above
+            if dollar_type == "yes":
+                profit_type = "$"
+            else:
+                profit_type = "%"
+
+        elif profit_type == "unknown" and amount < 100:
+            percent_type = yes_no("Do you mean {}kgs? , "
+                                  "y / n".format(amount))
+
+            if percent_type == "yes":
+                profit_type = "%"
+            else:
+                profit_type = "$"
+
+        # return profit goal to main routine
+        if profit_type == "$":
+            return amount
+        else:
+            goal = (amount / 100) * total_costs
+            return goal
+
+
+profit_target = profit_goal(100)
+print(profit_target)
